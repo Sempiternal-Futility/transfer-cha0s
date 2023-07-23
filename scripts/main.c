ac55c27557b23310ef98a63987b855a950757134
Sempiternal-Futility <88591666+Sempiternal-Futility@users.noreply.github.com>
2023-07-23 12:04:12 -0300

    Program can now know portN and structs are now pointers

--- scripts/main.c
+++ scripts/main.c
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool ask_host_type();
-void get_host_ipaddr(bool is_server, struct sockaddr_in server, struct sockaddr_in client);
+bool ask_host_type(); // Asks if the host is a server or a client
+void get_host_ipaddr(bool is_server, struct sockaddr_in *server, struct sockaddr_in *client); // Asks user for an IPv4 address
+void ask_portn(bool is_server, struct sockaddr_in *server, struct sockaddr_in *client); // Asks the user for a port number they want to connect
 
 int main()
 {
@@ -15,14 +16,19 @@ int main()
    curs_set(0);
    noecho();
 
-   struct sockaddr_in server;
-   struct sockaddr_in client;
+   struct sockaddr_in *server = malloc(sizeof(struct sockaddr_in));
+   struct sockaddr_in *client = malloc(sizeof(struct sockaddr_in));
+   server->sin_family = AF_INET;
+   client->sin_family = AF_INET;
 
    bool is_server = ask_host_type();
    get_host_ipaddr(is_server, server, client);
+   ask_portn(is_server, server, client);
 
    getch();
 
+   free(server);
+   free(client);
    endwin();
    return 0;
 }
@@ -61,7 +67,7 @@ bool ask_host_type()
    }
 }
 
-void get_host_ipaddr(bool is_server, struct sockaddr_in server, struct sockaddr_in client)
+void get_host_ipaddr(bool is_server, struct sockaddr_in *server, struct sockaddr_in *client)
 {
    clear();
    echo();
@@ -86,10 +92,42 @@ void get_host_ipaddr(bool is_server, struct sockaddr_in server, struct sockaddr_
    }
 
    if (is_server == true)
-      inet_pton(AF_INET, ip_addr_string, &(server.sin_addr));
+      inet_pton(AF_INET, ip_addr_string, &(server->sin_addr));
 
    else if (is_server == false)
-      inet_pton(AF_INET, ip_addr_string, &(client.sin_addr));
+      inet_pton(AF_INET, ip_addr_string, &(client->sin_addr));
+
+   noecho();
+}
+
+void ask_portn(bool is_server, struct sockaddr_in *server, struct sockaddr_in *client)
+{
+   clear();
+   echo();
+
+   move(LINES /2, (COLS /2 -12));
+   printw("WHICH PORT SHALL BE USED?");
+
+   move((LINES /2 +2), (COLS /2 -2));
+
+   char input;
+   char port_string[6];
+
+   for(short i = 0; i < 5; i++)
+   {
+      input = getch();
+      if (input == '\n')
+         break;
+      port_string[i] = input;
+   }
+
+   int port = atoi(port_string);
+
+   if (is_server == true)
+      server->sin_port = htons(port);
+
+   else
+      client->sin_port = htons(port);
 
    noecho();
 }
