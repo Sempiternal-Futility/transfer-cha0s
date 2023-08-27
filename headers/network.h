814dc71264064eb1d3d64179952fec7fd178df32
Sempiternal-Futility <88591666+Sempiternal-Futility@users.noreply.github.com>
2023-08-27 01:05:10 -0300

    Used if states to check if it's a file to send it or not

--- headers/network.h
+++ headers/network.h
@@ -120,85 +120,97 @@ void server_send(int sockfd)
       }
    }
 
-   char *file_name = basename(path);
-   bool is_ascii = is_file_ascii(path);
    bool is_file = is_path_file(path);
+   send(sockfd, &is_file, sizeof is_file, 0);
 
-   char *mode;
-
-   if (is_ascii == true)
-      mode = "r";
-   else if (is_ascii == false)
-      mode = "rb";
-
-   FILE *file = fopen(path, mode);
-
-   struct stat st;
-   stat(path, &st);
-   size_t file_size = st.st_size;
-
-   clear(); print_center("ALLOCATING BUFFERS...", 0, 21);
-
-   char *file_data = malloc(file_size); // This holds the contents of the file
-   char *tmp_buffer = 0; 
-   if (is_ascii == true)
-      tmp_buffer = malloc(file_size);
-
-   memset(file_data, 0, file_size);
-   if (is_ascii == true)
-      memset(tmp_buffer, 0, file_size);
-
-   clear(); print_center("READING FILE...", 0, 15);
-
-   if (is_ascii == true)
+   if (is_file == true)
    {
-      while (!feof(file)) {
-         strcat(file_data, tmp_buffer);
-         fgets(tmp_buffer, sizeof tmp_buffer, file);
+      char *file_name = basename(path);
+      bool is_ascii = is_file_ascii(path);
+
+      char *mode;
+
+      if (is_ascii == true)
+         mode = "r";
+      else if (is_ascii == false)
+         mode = "rb";
+
+      FILE *file = fopen(path, mode);
+
+      struct stat st;
+      stat(path, &st);
+      size_t file_size = st.st_size;
+
+      clear(); print_center("ALLOCATING BUFFERS...", 0, 21);
+
+      char *file_data = malloc(file_size); // This holds the contents of the file
+      char *tmp_buffer = 0; 
+      if (is_ascii == true)
+         tmp_buffer = malloc(file_size);
+
+      memset(file_data, 0, file_size);
+      if (is_ascii == true)
+         memset(tmp_buffer, 0, file_size);
+
+      clear(); print_center("READING FILE...", 0, 15);
+
+      if (is_ascii == true)
+      {
+         while (!feof(file)) {
+            strcat(file_data, tmp_buffer);
+            fgets(tmp_buffer, sizeof tmp_buffer, file);
+         }
+
+         file_data[strlen(file_data) -1] = '\0'; // This fix a bug where ascii files are outputted corrupted
       }
 
-      file_data[strlen(file_data) -1] = '\0'; // This fix a bug where ascii files are outputted corrupted
-   }
+      else if (is_ascii == false)
+         fread(file_data, 1, file_size, file);
 
-   else if (is_ascii == false)
-      fread(file_data, 1, file_size, file);
+      char ascii_buffer[1];
+      if (is_ascii == true)
+         ascii_buffer[0] = '1';
+      else if (is_ascii == false)
+         ascii_buffer[0] = '0';
 
-   char ascii_buffer[1];
-   if (is_ascii == true)
-      ascii_buffer[0] = '1';
-   else if (is_ascii == false)
-      ascii_buffer[0] = '0';
+      print_transfer_message(file_size);
 
-   print_transfer_message(file_size);
+      if (_conf_enable_ip_save == true && _is_ip_conf_empty == true && _failed_bind == false) // These are global vars btw
+         send_ip(sockfd); // Sends the ip addresses to the client (so he can save it on his machine), and also writes the ip's to this machine
 
-   if (_conf_enable_ip_save == true && _is_ip_conf_empty == true && _failed_bind == false) // These are global vars btw
-      send_ip(sockfd); // Sends the ip addresses to the client (so he can save it on his machine), and also writes the ip's to this machine
+      send(sockfd, ascii_buffer, sizeof ascii_buffer, 0); // Tells the client whether the file is ascii or binary
+      send(sockfd, file_name, sizeof file_name, 0); // Tells the client the name of the file
+      system("sleep 0.005s"); // This sleep is here to make sure the client has time to prepare for recving
+      send(sockfd, &file_size, sizeof file_size, 0); // Tells the client the size of the file
+     
+      long n = 0;
+      size_t total = 0;
+      while (total < file_size)
+      {
+         n = send(sockfd, &file_data[total], file_size - total, 0); // Sends the actual file for the client
+         total = total + n;
 
-   send(sockfd, ascii_buffer, sizeof ascii_buffer, 0); // Tells the client whether the file is ascii or binary
-   send(sockfd, file_name, sizeof file_name, 0); // Tells the client the name of the file
-   system("sleep 0.005s"); // This sleep is here to make sure the client has time to prepare for recving
-   send(sockfd, &file_size, sizeof file_size, 0); // Tells the client the size of the file
-  
-   long n = 0;
-   size_t total = 0;
-   while (total < file_size)
-   {
-      n = send(sockfd, &file_data[total], file_size - total, 0); // Sends the actual file for the client
-      total = total + n;
-
-      if (n == -1) {
-         perror("send() error"); 
-         getch();
-         break;
+         if (n == -1) {
+            perror("send() error"); 
+            getch();
+            break;
+         }
       }
-   }
 
-   free(file_data);
-   free(tmp_buffer); 
+      free(file_data);
+      free(tmp_buffer); 
+      
+      clear();
+      print_center("DONE!", 0, 5);
+      getch();
+   }
    
-   clear();
-   print_center("DONE!", 0, 5);
-   getch();
+   else if (is_file == false)
+   {
+      clear();
+      print_center("THE TRANSFERING OF FOLDERS HAS NOT YET BEEN IMPLEMENTED!", 0, 56);
+      getch();
+   }
 }
 
 void client_recv(int sockfd)
@@ -207,72 +219,85 @@ void client_recv(int sockfd)
    print_center("CONNECTED TO THE SERVER!", 0, 24);
    print_center("SERVER IS CHOOSING A FILE TO SEND...", 1, 34);
 
-   char ascii_buffer[1];
-   char *file_name = calloc(1, 255);
-   size_t file_size;
+   bool is_file = true;
+   recv(sockfd, &is_file, sizeof is_file, 0);
 
-   if (_conf_enable_ip_save == true && _is_ip_conf_empty == true && _failed_bind == false) // These are global vars btw
-      recv_ip(sockfd); // Receives the ip addresses from the server and writes it to this machine
-
-   recv(sockfd, ascii_buffer, sizeof ascii_buffer, 0); // Receives the ascii bool
-   recv(sockfd, file_name, sizeof file_name, 0); // Receives the file name
-   recv(sockfd, &file_size, sizeof file_size, 0); // Receives the file size from the server
-
-   clear(); print_center("ALLOCATING BUFFERS...", 0, 21);
-
-   char *file_data = malloc(file_size); // This holds the contents of the file
-   for (long unsigned int i = 0; i < sizeof file_data; i++) // Makes sure the array is empty
-      file_data[i] = 0;
-
-   print_transfer_message(file_size);
-
-   long n = 0;
-   size_t total = 0;
-   while (total < file_size)
+   if (is_file == true)
    {
-      n = recv(sockfd, &file_data[total], file_size - total, 0); // Receives the file from the server
-      total = total + n;
+      char ascii_buffer[1];
+      char *file_name = calloc(1, 255);
+      size_t file_size;
 
-      if (n == -1) {
-         perror("recv(): error");
-         getchar();
-         break;
+      if (_conf_enable_ip_save == true && _is_ip_conf_empty == true && _failed_bind == false) // These are global vars btw
+         recv_ip(sockfd); // Receives the ip addresses from the server and writes it to this machine
+
+      recv(sockfd, ascii_buffer, sizeof ascii_buffer, 0); // Receives the ascii bool
+      recv(sockfd, file_name, sizeof file_name, 0); // Receives the file name
+      recv(sockfd, &file_size, sizeof file_size, 0); // Receives the file size from the server
+
+      clear(); print_center("ALLOCATING BUFFERS...", 0, 21);
+
+      char *file_data = malloc(file_size); // This holds the contents of the file
+      for (long unsigned int i = 0; i < sizeof file_data; i++) // Makes sure the array is empty
+         file_data[i] = 0;
+
+      print_transfer_message(file_size);
+
+      long n = 0;
+      size_t total = 0;
+      while (total < file_size)
+      {
+         n = recv(sockfd, &file_data[total], file_size - total, 0); // Receives the file from the server
+         total = total + n;
+
+         if (n == -1) {
+            perror("recv(): error");
+            getchar();
+            break;
+         }
       }
-   }
 
-   bool is_ascii;
-   if (ascii_buffer[0] == '1')
-      is_ascii = true;
-   else if (ascii_buffer[0] == '0')
-      is_ascii = false;
+      bool is_ascii;
+      if (ascii_buffer[0] == '1')
+         is_ascii = true;
+      else if (ascii_buffer[0] == '0')
+         is_ascii = false;
 
-   if (is_ascii == true) // These 2 lines fix a bug where ascii files are outputted corrupted
+      if (is_ascii == true) // These 2 lines fix a bug where ascii files are outputted corrupted
+      {
+         file_data[strlen(file_data) +1] = '\0';
+         file_data[strlen(file_data)] = '\n';
+      }
+
+      char *mode;
+      if (is_ascii == true)
+         mode = "w";
+      else if (is_ascii == false)
+         mode = "wb";
+
+      clear(); print_center("WRITING FILE...", 0, 18);
+
+      FILE *file = fopen(file_name, mode);
+      if (is_ascii == true)
+         fprintf(file, file_data);
+
+      else if (is_ascii == false)
+         fwrite(file_data, 1, file_size, file);
+
+      free(file_name);
+      free(file_data);   
+
+      clear();
+      print_center("DONE!", 0, 5);
+      getch();
+   }   
+
+   else if (is_file == false)
    {
-      file_data[strlen(file_data) +1] = '\0';
-      file_data[strlen(file_data)] = '\n';
+      clear();
+      print_center("THE TRANSFERING OF FOLDERS HAS NOT YET BEEN IMPLEMENTED!", 0, 56);
+      getch();
    }
-
-   char *mode;
-   if (is_ascii == true)
-      mode = "w";
-   else if (is_ascii == false)
-      mode = "wb";
-
-   clear(); print_center("WRITING FILE...", 0, 18);
-
-   FILE *file = fopen(file_name, mode);
-   if (is_ascii == true)
-      fprintf(file, file_data);
-
-   else if (is_ascii == false)
-      fwrite(file_data, 1, file_size, file);
-
-   free(file_name);
-   free(file_data);   
-
-   clear();
-   print_center("DONE!", 0, 5);
-   getch();
 }
 
 #endif
